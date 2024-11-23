95272db64cd4caed3ec2dfc84999286bb802e757
sdSAHABUDDIN <syedsahabuddin182@gmail.com>
2024-11-23 20:42:17 +0530

    create all router and modify main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Navber from './components/Navber/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Cart from './pages/cart/Cart'
+import Order from './pages/Order/Order'
+
 
 function App() {
   return (
     <div className='app'>
       <Navber />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<Order/>}/>
+        
+        
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/cart/Cart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Cart.css'
+function cart() {
+  return (
+    <div>cart</div>
+  )
+}
+
+export default cart
