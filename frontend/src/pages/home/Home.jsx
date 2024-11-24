e362140fea32093657cd52bf25aed39f11134dfd
sdSAHABUDDIN <syedsahabuddin182@gmail.com>
2024-11-24 08:56:25 +0530

    Fooditem add and style it

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/foodDisplay/FoodDisplay'
 function Home() {
+  const [category,setCategory]=useState("All")
   return (
     <div><Header />
-    <ExploreMenu /></div>
+    <ExploreMenu category={category} setCategory={setCategory}/>
+    <FoodDisplay category={category}/>
+    </div>
   )
 }
 
